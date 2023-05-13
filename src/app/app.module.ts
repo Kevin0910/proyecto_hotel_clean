eb6420749986046cc63cac84eaab6ada97f2beb0
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2023-05-13 02:17:49 -0600

    Se agregaron los componentes como el navegador, personal, producto, servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
+import { PersonalComponent } from './components/personal/personal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ProductoComponent,
+    ServicioComponent,
+    PersonalComponent
   ],
   imports: [
     BrowserModule,
