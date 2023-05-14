1d8645b5cffe9c453eef5e15d03417143616bae4
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2023-05-13 18:02:37 -0600

    Se agregaron las rutas para navegar y se modifico el diseño de la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonalComponent } from './components/personal/personal.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PersonalComponent, pathMatch: 'full'},
+  {path: 'page-personal', component: PersonalComponent},
+  {path: 'page-producto', component: ProductoComponent},
+  {path: 'page-servicio', component: ServicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
