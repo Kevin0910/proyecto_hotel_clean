ee1d9efe4a48a0177283afe1ccbfa443fbfca917
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2023-06-01 02:17:22 -0600

    Se actualizo la busqueda de servicio, pero no termiando el de editar

--- src/app/components/servicio/formulario-servicio/formulario-servicio.component.ts
+++ src/app/components/servicio/formulario-servicio/formulario-servicio.component.ts
@@ -16,7 +16,7 @@ import swal from 'sweetalert2';
 })
 export class FormularioServicioComponent {
 
-  public serviciosARealizar: ObjetoServicioARealizar = new ObjetoServicioARealizar();
+  public servicioARealizar: ObjetoServicioARealizar = new ObjetoServicioARealizar();
 
   public tipoDeServicios: ObjetoServicio[];
   public listaClientes: ObjetoCliente[];
@@ -33,12 +33,13 @@ export class FormularioServicioComponent {
 
   ngOnInit() {
 
+    this.cargarServicioARealizar();
+
     this.clientesFiltrados = this.autoCompletado.valueChanges
     .pipe(
       map( value =>typeof value === 'string' ? value: value),
       flatMap(value => value ? this._filter(value || ''):[ ]),
     );
-
 
     this.pageServicioService.getListaServicio().subscribe(tipoDeServicios => this.tipoDeServicios = tipoDeServicios );
     this.clienteService.getClientes().subscribe(listaClientes => this.listaClientes = listaClientes);
@@ -50,11 +51,22 @@ export class FormularioServicioComponent {
     }
 
 
+    // ! Cargar datos del formulario
+  cargarServicioARealizar(): void{
+    this.activatedRoute.params.subscribe(params => {
+      let folio = params['folio']
+      if(folio){
+        this.pageServicioService.getServicioARealizar(folio).subscribe( (serviciosARealizar) => this.servicioARealizar = serviciosARealizar)
+      }
+    })
+  }
+
+
 
     //! CREAR
     create(): void{
-      console.log(this.serviciosARealizar);
-      this.pageServicioService.create(this.serviciosARealizar).subscribe(
+      console.log(this.servicioARealizar);
+      this.pageServicioService.create(this.servicioARealizar).subscribe(
       serviciosARealizar => {
         this.router.navigate(['/page-servicio'])
         swal('Servicio Guardado', `El servicio del cliente ${serviciosARealizar.cliente.nombre} ${serviciosARealizar.cliente.apellido1} se a guardado con exito`, 'success')
@@ -64,11 +76,11 @@ export class FormularioServicioComponent {
 
     //! MODIFICAR
     update():void{
-      console.log(this.serviciosARealizar)
-      this.pageServicioService.update(this.serviciosARealizar)
-      .subscribe( serviciosARealizar => {
+      //console.log(this.serviciosARealizar)
+      this.pageServicioService.update(this.servicioARealizar)
+      .subscribe( servicioARealizar => {
           this.router.navigate(['/page-servicio'])
-          swal ('Servicio Guardado', `El servicio del cliente ${serviciosARealizar.cliente.nombre} ${serviciosARealizar.cliente.apellido1} se ha actualizado con exito`, 'success' )
+          swal ('Servicio Guardado', `El servicio del cliente ${servicioARealizar.cliente.nombre} ${servicioARealizar.cliente.apellido1} se ha actualizado con exito`, 'success' )
         }
         // err =>{
         //   this.errores = err.error.errors as string[];
--- src/app/services/page-servicio.service.ts
+++ src/app/services/page-servicio.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, catchError, throwError } from 'rxjs';
 import { ObjetoServicio } from '../interfaces/servicio';
 import { ObjetoServicioARealizar } from '../interfaces/servicioARealizar';
@@ -18,16 +18,33 @@ export class PageServicioService {
   private HttpHeaders = new HttpHeaders ({'Content-Type': 'application/json'})
 
   constructor(private http: HttpClient,
-              private router: Router) { }
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   getListaServicio(): Observable<ObjetoServicio[]>{
     return this.http.get<ObjetoServicio[]>(`${this.urlEndPointListaServicio}/lista-servicios`)
   }
 
-
   getServicios(): Observable<ObjetoServicioARealizar[]>{
     return this.http.get<ObjetoServicioARealizar[]>(`${this.urlEndPoint}/lista-servicio-a-realizar`)
   }
+
+
+  // ! OBTENER POR ID
+  getServicioARealizar(folio): Observable<any>{
+    return this.http.get<any>(`${this.urlEndPoint}/editar-servicio/${folio}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/page-servicio']);
+        console.error(e.error.mensaje);
+        swal('Error al editar', e.error.mensaje, 'error');
+        return throwError(()=>e)
+      })
+    );
+  }
+
+  // getServiciosRealizar(folio):Observable<ObjetoServicioARealizar[]>{
+  //   return this.http.get<ObjetoServicioARealizar[]>(`${this.urlEndPoint}/editar-servicio/${folio}`)
+  // }
 
   busquedaServicioCliente(termino: string): Observable<ObjetoServicioARealizar[] | null>{
     return this.http.get<ObjetoServicioARealizar[]>(`${this.urlEndPoint}/servicio-a-realizar-cliente/${termino}`)
@@ -55,21 +72,10 @@ export class PageServicioService {
     );
   }
 
-  // ! OBTENER POR ID
-  getServicioARealizar(folio): Observable<any>{
-    return this.http.get<any>(`${this.urlEndPoint}/editar-servicio/${folio}`).pipe(
-      catchError(e => {
-        this.router.navigate(['/page-servicio']);
-        console.error(e.error.mensaje);
-        swal('Error al editar', e.error.mensaje, 'error');
-        return throwError(()=>e)
-      })
-    );
-  }
 
   // ! MODIFICAR
-  update(objetoServicioARealizar: ObjetoServicioARealizar): Observable<ObjetoServicioARealizar>{
-    return this.http.put<ObjetoServicioARealizar>(`${this.urlEndPoint}/editar-servicio/${objetoServicioARealizar.folio}`, objetoServicioARealizar, {headers:this.HttpHeaders}).pipe(
+  update(objetoServicioARealizar: ObjetoServicioARealizar): Observable<any>{
+    return this.http.put<any>(`${this.urlEndPoint}/editar-servicio/${objetoServicioARealizar.folio}`, objetoServicioARealizar, {headers:this.HttpHeaders}).pipe(
         catchError(e => {
 
           if(e.status == 400){
